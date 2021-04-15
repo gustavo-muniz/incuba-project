1b7310667b8cd4bc5c05d7aa40ae2f3fb70ddc00
Gustavo Muniz <gustamunizbr@gmail.com>
2021-04-15 20:16:53 -0300

    added specification of HTTP methods allowed for controller actions

--- grails-app/controllers/com/market/ProductsController.groovy
+++ grails-app/controllers/com/market/ProductsController.groovy
@@ -3,36 +3,46 @@ import grails.converters.JSON;
 
 class ProductsController {
 
+    def productService;
+    
+    static allowedMethods = [ index: "GET", 
+        show: "GET", 
+        store: "POST",
+        destroy: "DELETE",
+        edit: "PATCH",
+        set_status: "PATCH"
+    ]
+
     def index() { 
-        def result = ProductService.list();
+        def result = productService.list();
         render result as JSON
     }
 
     def show() {
         def received = request.JSON;
-        def result = ProductService.get(received.id);
+        def result = productService.get(received.id);
         render result as JSON
     }
 
     def store() {
         def received = request.JSON;
-        def result = ProductService.create(received);
+        def result = productService.create(received);
         render result as JSON
     }
 
     def destroy() {
         def received = request.JSON;
-        def result = ProductService.remove(received.id);
+        def result = productService.remove(received.id);
         render result as JSON
     }
     def edit() {
         def received = request.JSON;
-        def result = ProductService.edit(received.id);
+        def result = productService.edit(received.id);
         render result as JSON
     }
     def set_status() {
         def received = request.JSON;
-        def result = ProductService.status(received);
+        def result = productService.status(received);
         render result as JSON
     }
 }
