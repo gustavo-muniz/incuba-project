d468bd5f81235bfd33bfcdaa36a984ddd9c21565
Gustavo Muniz <gustamunizbr@gmail.com>
2021-04-15 20:15:11 -0300

    created RESTful URL mappings

--- grails-app/controllers/incubaproject/UrlMappings.groovy
+++ grails-app/controllers/incubaproject/UrlMappings.groovy
@@ -3,6 +3,12 @@ package incubaproject
 class UrlMappings {
 
     static mappings = {
+        
+        get "/products"(controller: "products", action: "index")
+        post "/products"(controller: "products", action: "store")
+        get "/products/$id"(controller: "products", action: "show")
+        delete "/products/$id"(controller: "products", action: "destroy")
+
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
